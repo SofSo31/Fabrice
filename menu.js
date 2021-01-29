f3f05af0cff7ef946746c33efca0b02da352bff1
SofSo31 <78239871+SofSo31@users.noreply.github.com>
2021-01-30 00:59:02 +0100

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login("ODA0ODU3MzE2MTAwMDE0MTAw.YBSbyQ.EwMRF1EHR8-6Fgp8QcI9ERPpej8")
+
+var prefix = ("*");
+
+client.on("message", (message) => {
+
+    if(message.content === "créateur") {
+        message.channel.send("丂のｷ丂の#6229")
+    }
+
+    if(message.content === prefix + "help"){
+        message.channel.send("Veux-tu de l'aide?")
+    }
+
+
+});
