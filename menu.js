21c0b2a912b09674b9f35eb99a1b4377d1be08c2
SofSo31 <78239871+SofSo31@users.noreply.github.com>
2021-01-30 01:11:38 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login(process.env.TOKEN);
+client.login(ODA0ODU3MzE2MTAwMDE0MTAw.YBSbyQ.EwMRF1EHR8-6Fgp8QcI9ERPpej8);
 
 var prefix = ("*");
 
