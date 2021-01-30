cee3e8db4d5296414fd94f085c6f31d180d522fa
SofSo31 <78239871+SofSo31@users.noreply.github.com>
2021-01-30 01:26:56 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,11 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login(process.env.TOKEN);
+client.login("process.env.TOKEN");
 
 var prefix = ("*");
 
 client.on("message", (message) => {
+    console.log("Le bot est allumé");
+    client.user.setStatus("dnd");
+    client.user.setActivity("En développement | *help");
 
     if(message.content === prefix + "créateur") {
         message.channel.send("丂のｷ丂の#6229")
